3918d0a8f3773fe010484b9d78e6e33f5da4a36d
numonofcv <numonofcv@gmail.com>
2026-02-17 03:59:32 +0500

    fixed 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: 'export',
-  basePath: '/BoroBazar',
+  output: isProd ? "export" : undefined,
+  basePath: isProd ? "/BoroBazar" : "",
   images: {
     unoptimized: true,
   },
