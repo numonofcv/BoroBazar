5f30f3cfb910f19c598f39b78a7102f2a034fbf6
numonofcv <numonofcv@gmail.com>
2026-02-17 11:48:54 +0500

    Updates

--- _next/static/yB6jW56P1tg-eo5mw0Q3P/_buildManifest.js
+++ _next/static/yB6jW56P1tg-eo5mw0Q3P/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/BoroBazar//_next/:path+",
+        "destination": "/BoroBazar/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
