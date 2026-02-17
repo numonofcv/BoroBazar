a8c70b2f047f6fd84cb17f867b940a2c62d23bfb
numonofcv <numonofcv@gmail.com>
2026-02-17 11:51:14 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: isProd ? "export" : undefined,
   basePath: isProd ? "/BoroBazar" : "",
+  assetPrefix: isProd ? "/BoroBazar/" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
